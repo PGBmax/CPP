07b3ed134ac34f2e16272392614a9fef9162539e
Pierre Boucher <pboucher@2D2.42angouleme.fr>
2025-03-20 18:14:35 +0100

    Megaphone and Phonebook

--- cpp0/ex01/phonebook.cpp
+++ cpp0/ex01/phonebook.cpp
@@ -6,7 +6,7 @@
 /*   By: pboucher <pboucher@42student.fr>           +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/03/20 16:48:42 by pboucher          #+#    #+#             */
-/*   Updated: 2025/03/20 17:33:39 by pboucher         ###   ########.fr       */
+/*   Updated: 2025/03/20 18:12:40 by pboucher         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,12 +14,29 @@
 
 static void	add_to_phonebook(void)
 {
-	std::cout << RED "Oh, you want to add someone in the Phonebook ?" << std::endl;
-	std::cout << "first name       > John" << std::endl;
-	std::cout << "last name        > Pork" << std::endl;
-	std::cout << "nickname         > Johnny" << std::endl;
-	std::cout << "phone number     > 06 69 69 69 69" << std::endl;
-	std::cout << "darkest secret   > He eats pig" RESET << std::endl;
+	std::cout << RED "First Name     > John" << std::endl;
+	std::cout << "Last Name      > Pork" << std::endl;
+	std::cout << "Nickname       > Johnny" << std::endl;
+	std::cout << "Phone Number   > 06 69 69 69 69" << std::endl;
+	std::cout << "Darkest Secret > He is racist" RESET << std::endl;
+}
+
+static void print_phonebook(void)
+{
+	std::cout << BLUE;
+	std::cout << "╔══════════╦══════════╦══════════╦══════════╗" << std::endl;
+	std::cout << "║Person  ID║First Name║Last  Name║ Nickname ║" << std::endl;
+	std::cout << "╠══════════╬══════════╬══════════╬══════════╣" << std::endl;
+	std::cout << "║        1 ║     John ║     Pork ║   Johnny ║" << std::endl;
+	std::cout << "║          ║          ║          ║          ║" << std::endl;
+	std::cout << "║          ║          ║          ║          ║" << std::endl;
+	std::cout << "║          ║          ║          ║          ║" << std::endl;
+	std::cout << "║          ║          ║          ║          ║" << std::endl;
+	std::cout << "║          ║          ║          ║          ║" << std::endl;
+	std::cout << "║          ║          ║          ║          ║" << std::endl;
+	std::cout << "║          ║          ║          ║          ║" << std::endl;
+	std::cout << "╚══════════╩══════════╩══════════╩══════════╝" << std::endl;
+	std::cout << RESET;
 }
 
 int main(void)
@@ -32,6 +49,8 @@ int main(void)
 		std::cin >> input;
 		if (input == "ADD")
 			add_to_phonebook();
+		else if (input == "SEARCH")
+			print_phonebook();
 		else if (input == "EXIT")
 		{
 			std::cout << MAGENTA "Bye bye from the phonebook 🥰" RESET << std::endl;
